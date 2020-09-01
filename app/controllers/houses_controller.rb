aeeb744aa482875bdb4eed554469e2d631929c37
Branca Chiotte <brancachiotte@gmail.com>
2020-09-01 11:07:09 +0100

    need to stop issues with master

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,2 +1,51 @@
 class HousesController < ApplicationController
+  before_action :set_house, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @houses = House.all
+  end
+
+  def show
+  end
+
+  def new
+    @house = House.new
+  end
+
+  def create
+    @house = House.new(house_params)
+    if @house.save
+      redirect_to house_path(@house)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @house = house.update(house_params)
+    if @house.save
+      redirect_to house_path(@house)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @house.destroy
+    redirect_to house_path
+  end
+
+  private
+
+  def set_house
+    @house = House.find(params[:id])
+  end
+
+  def house_params
+    params.require(:house).permit(:house_balance, :name, :email)
+  end
+
 end
--- app/models/house.rb
+++ app/models/house.rb
@@ -1,4 +1,8 @@
 class House < ApplicationRecord
   has_many :memberships
   has_many :users, through: :memberships
+
+  validates :house_balance, :name, :email, presence: true
+  validates :house_balance, numericality: true
+  validates :email, uniqueness: true
 end
