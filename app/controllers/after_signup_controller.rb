2e41ab545635ea45ece535dc9dc118c7b302cc8b
Maria Nery <mnvnery@gmail.com>
2020-09-01 15:10:56 +0100

    registration step 1

---
+++ app/controllers/after_signup_controller.rb
@@ -0,0 +1,11 @@
+class AfterSignupController < ApplicationController
+  include Wicked::Wizard
+
+  steps :create_house, :add_members
+
+  def show
+    @user = current_user
+    @house = House.new
+    render_wizard
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :houses
+  resources :after_signup
 end
