2d4eecc95f99988c39e14a93579f2522204f40fd
Maria Nery <mnvnery@gmail.com>
2020-09-01 10:58:50 +0100

    log in and sign up review

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
