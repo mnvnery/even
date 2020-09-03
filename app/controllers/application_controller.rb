44833e12b16c8d31256ea907e8e921fa09eccb5e
Sini Ninkovic <sini.sn.ninkovic@gmail.com>
2020-09-03 14:17:46 +0100

    users can only see their shares, no other shares. user only sees shares associated to the current_house. User can see dashboard with everybody's bills associated to current house

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, only: :home
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_variables
 
   def home
   end
+
+  def set_variables
+    @symbol_map = {tennis: 'blue-square.svg', soccer: 'green-halfcircle.svg', basketball: 'red-halfcircle.svg'}
+  end
+
+
 
   protected
 
@@ -11,3 +18,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
+
+
+
+
--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,8 +1,12 @@
 class SharesController < ApplicationController
 
   def index
+    @shares_all = Share.all
+    # Problems with nil class solved with if statement in view:
+    # @house_shares = Share.joins(:bills).where(bills: {house_id: @house})
     @shares = Share.where(user: current_user)
     @house = House.find(params[:house_id])
+
   end
 
   def edit
@@ -25,3 +29,4 @@ private
   end
 
 end
+
