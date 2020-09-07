7a340b253811fbaaef79f3ecf4e860ef78570447
Sini Ninkovic <sini.sn.ninkovic@gmail.com>
2020-09-07 17:50:20 +0100

    paid shares no longer show up on the index pages or stripe payment pages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+
+  config.hosts << /[a-z0-9]+\.ngrok\.io/
+
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
   # Settings specified here will take precedence over those in config/application.rb.
 
