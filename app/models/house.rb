0ce7cce5d21cac986e46b579c027a811708bdc6c
Maria Nery <mnvnery@gmail.com>
2020-09-08 16:46:42 +0100

    heroku pushing

--- app/models/house.rb
+++ app/models/house.rb
@@ -2,6 +2,7 @@ class House < ApplicationRecord
   has_many :memberships
   has_many :invitations
   has_many :users, through: :memberships
+  has_many :bills, dependent: :destroy
 
   validates :name, presence: true
 
