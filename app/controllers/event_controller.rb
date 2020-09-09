7c4033a92238e0db9f628ec007ceebec0f9a8f60
Sini Ninkovic <sini.sn.ninkovic@gmail.com>
2020-09-09 17:23:08 +0100

    created a little gamified system to incentivice people to pay more frequently. it calculates to slowest payer but the code is not clean yet and there is a table that needs to be added to show all things a snail can potentially do

---
+++ app/controllers/event_controller.rb
@@ -0,0 +1,10 @@
+class EventController < ApplicationController
+
+   # //this action gets called after a user creates their Stripe acct
+   def stripe_callback
+   end
+
+   # //this is where a user will be able to see their balance info
+   def payment_profile
+   end
+end
